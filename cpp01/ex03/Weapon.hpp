3de6aed0783afa93cbc5299cc6a4380af63f9169
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-04-09 19:54:10 +0200

    add cpp01-ex03

---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,20 @@
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <string>
+
+class Weapon
+{
+
+private:
+	std::string type;
+
+public:
+	Weapon(std::string type);
+
+	const std::string &getType();
+	void setType(const std::string &type);
+
+};
+
+#endif
