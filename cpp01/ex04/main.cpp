7a349fa58f546ad610bf64960d6ab18772f3451e
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2024-04-10 13:57:13 +0200

    add cpp01-ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 4)
+	{
+		std::cerr << "Usage: filename s1 s2" << std::endl;
+		return 1;
+	}
+
+	std::string filename = std::string(argv[1]);
+	std::string s1 = std::string(argv[2]);
+	std::string s2 = std::string(argv[3]);
+	std::string content;
+
+	std::ifstream ifile(filename.c_str());
+	std::ofstream ofile(filename.append(".replace").c_str());
+	if (!ifile.is_open() || !ofile.is_open())
+	{
+		std::cerr << "Couldn't open file " << argv[1] << std::endl;
+		return 2;
+	}
+
+	while (std::getline(ifile, content))
+	{
+
+		size_t index = content.find(s1);
+		while (index != std::string::npos)
+		{
+			content = content.erase(index, s1.length());
+			content = content.insert(index, s2);
+			index = content.find(s1);
+		}
+		ofile << content << std::endl;
+	}
+	return 0;
+}
